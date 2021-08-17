19cad5b5c91227585702e77524de0a8e0f9e288a
Tiatholait <63349292+VovaLite1@users.noreply.github.com>
2021-08-17 07:32:15 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,25 @@
+// подключаем модуль датчика уровня воды
+var level = require('@amperka/water-level');
+// подключаем датчик на дне бочки
+var onBottom = level.connect(P1, {debounce: 3});
+// подключаем датчик на крышке бочки
+var onTop = level.connect(P2, {debounce: 3});
+var pump = require('@amperka/relay').connect(P5);
+
+var switchOn = function() {
+  // включаем насос
+  pump.turnOn();
+};
+// событие: датчик на крышке бочки поднялся
+onTop.on('up', function () {
+  print('water level is high');
+  // выключаем устройство
+  switchOff();
+});
+ 
+// событие: датчик на дне бочки опустился
+onBottom.on('down', function () {
+  // включаем устройство
+  switchOn();
+  print('water level is low');
+});
