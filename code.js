4a88050b2ad90df9c11b313b92a1738fc9d86406
Tiatholait <63349292+VovaLite1@users.noreply.github.com>
2021-08-17 09:42:40 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -5,21 +5,32 @@ var onBottom = level.connect(P1, {debounce: 3});
 // подключаем датчик на крышке бочки
 var onTop = level.connect(P2, {debounce: 3});
 var pump = require('@amperka/relay').connect(P5);
+var myButton = require('@amperka/button').connect(P4, {});
 
 var switchOn = function() {
   // включаем насос
   pump.turnOn();
+  print('On');
 };
+
+var switchOff = function() {
+  // выключаем насос
+  pump.turnOff();
+  print('Off');
+};
+
 // событие: датчик на крышке бочки поднялся
 onTop.on('up', function () {
   print('water level is high');
   // выключаем устройство
   switchOff();
 });
- 
+
 // событие: датчик на дне бочки опустился
 onBottom.on('down', function () {
   // включаем устройство
   switchOn();
   print('water level is low');
 });
+
+
