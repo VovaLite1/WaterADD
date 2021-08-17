8309253e978a4e3bfee99ee7e8642d10a3b6847e
Ynelo <63349292+VovaLite1@users.noreply.github.com>
2021-08-17 13:48:03 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -2,8 +2,7 @@
 var level = require('@amperka/water-level');
 // подключаем датчик на дне бочки
 var onBottom = level.connect(P1, {debounce: 3});
-// подключаем датчик на крышке бочки
-var onTop = level.connect(P2, {debounce: 3});
+
 var pump = require('@amperka/relay').connect(P5);
 
 var switchOn = function() {
@@ -18,13 +17,6 @@ var switchOff = function() {
   print('Off');
 };
 
-// событие: датчик на крышке бочки поднялся
-onTop.on('up', function () {
-  print('water level is high');
-  // выключаем устройство
-  switchOff();
-});
-
 // событие: датчик на дне бочки опустился
 onBottom.on('down', function () {
   // включаем устройство
@@ -32,4 +24,9 @@ onBottom.on('down', function () {
   print('water level is low');
 });
 
+onBottom.on('up', function () {
+  // включаем устройство
+  switchOff();
+  print('water level is good');
+});
 
